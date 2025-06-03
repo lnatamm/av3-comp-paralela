cfda48e98efa87647e941bd0bc8d9152919d27d5
lnatamm <lnatamm25@gmail.com>
2025-06-02 21:50:15 -0300

    Adicionando comparação com serial

--- main.py
+++ main.py
@@ -24,3 +24,14 @@ print("Resultado da multiplicação:")
 for row in result:
     print(row)
 print(f"Tempo de execução paralelo: {time.time() - tempo_paraleo:.2f} segundos")
+
+tempo_serial = time.time()
+result_serial = []
+for i in range(len(M_1)):
+    result_serial.append([])
+    for j in range(len(M_2[0])):
+        result_serial[i].append(0)
+        for k in range(len(M_2)):
+            result_serial[i][j] += M_1[i][k] * M_2[k][j]
+
+print(f"Tempo de execução serial: {time.time() - tempo_serial:.2f} segundos")
